2c43ea51b4eb4c923be8d060b0f06903dd85f133
nemiro-TECH <69482731+nemiro-TECH@users.noreply.github.com>
2023-01-05 06:52:56 -0300

    Create main.f95

---
+++ code/main.f95
@@ -0,0 +1,8 @@
+! 
+! 
+!                             Black Hole G-Force
+!                 Code created for Educational Purpose only
+!            No warranty are givven or liability can be expected.
+!                       Licenced under MIT License.
+!                          Always give credits.
+! 
